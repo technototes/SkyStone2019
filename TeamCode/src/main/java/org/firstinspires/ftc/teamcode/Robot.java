90c2a99415a7ad42496aec56b8f4970e0d7ba293
Kevin Frei <freik@fb.com>
2019-10-17 16:53:58 -0700

    Skeleton code progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,27 +1,59 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Robot {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Basic: Robot op mode", group="Linear Opmode")
+public class Robot extends LinearOpMode {
+
+  private DcMotor linearSlideMotor = null;
+  private Servo grab = null;
+  private Servo claw = null;
+  private TouchSensor extended = null;
+  private TouchSensor retracted = null;
+  private ElapsedTime runtime = new ElapsedTime();
+
+
+  public Robot() {
+    linearSlideMotor = hardwareMap.get(DcMotor.class, "lslide");
+    grab = hardwareMap.get(Servo.class, "grabTurn");
+    claw = hardwareMap.get(Servo.class, "claw");
+    extended = hardwareMap.get(TouchSensor.class, "extLimitSwitch");
+    retracted = hardwareMap.get(TouchSensor.class, "retLimitSwitch");
+  }
+
+  @Override
+  public void runOpMode() throws InterruptedException {
+
+  }
 
   // Linear slide stuff:
   public boolean isLinearSlideFullyExtended() {
-    // TODO: Read the linear slide limit switch
-    return false;
+    return extended.isPressed();
   }
   public boolean isLinearSlideFullyRetracted() {
-    // TODO: Read the linear slide retract switch
-    return false;
+    return retracted.isPressed();
   }
   public void lslide(LinearSlideOperation dir) {
     // Set the linear slide motor to the position
     if (dir == LinearSlideOperation.Extend) {
-      // TODO: Fill this in
+      // TODO: This is probably wrong & silly
+      linearSlideMotor.setPower(1.0);
     } else if (dir == LinearSlideOperation.Retract) {
-      // TODO: Fill this in
+      // TODO: This is probably wrong & silly
+      linearSlideMotor.setPower(-1.0);
     }
   }
 
   // Grabber stuff:
   public void grabberClutch(GrabberMotorOperation operation) {
+    if (operation == GrabberMotorOperation.Close) {
+      //claw.
+    }
   }
   public GrabberPosition getGrabberPosition() {
     return GrabberPosition.Horizontal;
