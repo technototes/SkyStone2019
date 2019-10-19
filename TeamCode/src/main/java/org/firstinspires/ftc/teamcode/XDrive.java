e539530fd89a80788640ef074a5d190aed919a22
Kevin Frei <kevinfrei@hotmail.com>
2019-10-19 11:32:35 -0700

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.*;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+
 public class XDrive {
     public static final double SCALEFACTOR = 0.5;//turn speed factor
     private Robot robot;
@@ -27,6 +29,10 @@ public class XDrive {
     public static double rlPower;
     public static double tturn;
     //leave gyroAngle at zero to set relative angle
+    public void joystickDrive(Direction j1, Direction j2, double gyroAngle){
+        double hypotenuse = Functions.pyt(j1.X, j1.Y);
+        drive(Math.acos(j1.X/hypotenuse), gyroAngle, Range.clip(hypotenuse, -1.0, 1.0), j2.X);
+    }
     public void drive(double joystickAngle, double gyroAngle, double power, double turn) {
         tturn = turn*SCALEFACTOR;
         double angle = joystickAngle+robot.gyro();
@@ -40,24 +46,29 @@ public class XDrive {
         robot.motorRearRight(rrPower);
     }
     //set nearestSnap to true to snap to nearest 90 dgree angle, or set nearestSnap to false and input angle to snap to.
-    public double snapToAngle(boolean nearestSnap, double gyroAngle, double snapTo){
+    public double snapToAngle(double gyroAngle){
         double test = 0;
-        if(nearestSnap){
-            if(gyroAngle>50&&gyroAngle<130){
-                return 90;
+        bb:
+        if(true) {
+            if (gyroAngle > 50 && gyroAngle < 130) {
+                test = 90 - gyroAngle;
+                break bb;
             }
-            if(gyroAngle>140&&gyroAngle<220){
-                return 180;
+            if (gyroAngle > 140 && gyroAngle < 220) {
+                test = 180 - gyroAngle;
+                break bb;
             }
-            if(gyroAngle>230&&gyroAngle<310){
-                return 270;
+            if (gyroAngle > 230 && gyroAngle < 310) {
+                test =  270 - gyroAngle;
+                break bb;
             }
-            if((gyroAngle>=0&&gyroAngle<40)|| (gyroAngle>320&&gyroAngle<=360)){
-                return 0;
+            if ((gyroAngle >= 0 && gyroAngle < 40) || (gyroAngle > 320 && gyroAngle <= 360)) {
+                test = 0 - gyroAngle;
+                break bb;
             }
-        }else{
-
+            test = gyroAngle;
         }
+        return test;
     }
 }
 
