cf0c494c8187d6a1d93b82baa04b29392dacf8bc
s-kaoe <s-kaoe@bsd405.org>
2019-10-24 17:55:50 -0700

    Put in code for reading  direction from joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkeletonCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkeletonCode.java
@@ -59,92 +59,97 @@ public class SkeletonCode {
                         }
                     }
                 }
-            }
+                double angle = controller.readStick();
+                Direction L = controller.readLStick();
+                Direction R = controller.readRStick();
+                if (L.x > 0.01 || L.x < -0.01 || L.y > 0.01 || L.y < 0.01 || -0.01 < R.x  || R.x > 0.01) {
+                    robot.joystickDrive(L, R, robot.gyroHeading());
+                }
 //Drivecontrol
 
 //voidlinearSlide()
 //    while(true)
 
-            // {//therobotoverthebaseplate:)
-            pos = control.Rstick();
-            if (pos.x != 0) {
-                dt.rotate(pos.x);
+                // {//therobotoverthebaseplate:)
+                pos = control.Rstick();
+                if (pos.x != 0) {
+                    dt.rotate(pos.x);
 //}
-                pos = control.Lstick();
-                double x = pos.x;
-                double y = pos.y;
+                    pos = control.Lstick();
+                    double x = pos.x;
+                    double y = pos.y;
 //Emily
-                double power = Math.sqrt(x * x + y * y);
-                double angle = Math.acos(x / power);
-                dt.drive(power, angle, robot.gyro);
+                    double power = Math.sqrt(x * x + y * y);
+                    double angle = Math.acos(x / power);
+                    dt.drive(power, angle, robot.gyro);
 
-            }
-
-        }
-    }
-
-
-    void grabber() {
-        // ASSERT(we're already at the brick)
-        if (control.buttonA() == Button.Pressed) {
-            if (robot.getGrabberPosition() == GrabberPosition.Vertical) {
-                robot.setGrabberPosition(GrabberPosition.Horizontal);
-            } else { // It's HORIZONTAL!
-                robot.setGrabberPosition(GrabberPosition.Vertical);
-            }
-        }
-        if (control.buttonX() == Button.Pressed) {
-            robot.grabberClutch(GrabberMotorOperation.Open);
-        } else if (control.buttonY() == Button.Pressed) {
-            robot.grabberClutch(GrabberMotorOperation.Close);
-        } else {
-            robot.grabberClutch(GrabberMotorOperation.Off);
-        }
-    }
-
-    void linearSlide() {
-        while (true) { // the robot over the baseplate :)
-            Direction dpadDirection = control.dpad();
-            if (dpadDirection.isRight()) {
-                if (robot.isLinearSlideFullyExtended() == true) {
-                    robot.lslide(LinearSlideOperation.Off);
-                } else if (robot.isLinearSlideFullyRetracted() == true) {
-                    robot.lslide(LinearSlideOperation.Off);
-                } else {
-                    robot.lslide(LinearSlideOperation.Extend);
                 }
-            } else if (dpadDirection.isLeft()) {
-                if (robot.isLinearSlideFullyExtended() == true) {
-                    robot.lslide(LinearSlideOperation.Off);
-                } else if (robot.isLinearSlideFullyRetracted() == true) {
-                    robot.lslide(LinearSlideOperation.Off);
-                } else {
-                    robot.lslide(LinearSlideOperation.Retract);
+
+            }
+        }
+
+
+        void grabber () {
+            // ASSERT(we're already at the brick)
+            if (control.buttonA() == Button.Pressed) {
+                if (robot.getGrabberPosition() == GrabberPosition.Vertical) {
+                    robot.setGrabberPosition(GrabberPosition.Horizontal);
+                } else { // It's HORIZONTAL!
+                    robot.setGrabberPosition(GrabberPosition.Vertical);
                 }
             }
-        }
-    }
-
-    void lift() {
-        while (true) {
-            Direction dir = control.lstick();
-            if (dir.isUp()) {
-                // Pushed up:
-                if (robot.isFourBarUpperLimit() == true) {
-                    // HIT THEM LIMIT!
-                    robot.fourBarMotor(FourBarDirection.Off);
-                } else {
-                    robot.fourBarMotor(FourBarDirection.Up);
-                }
+            if (control.buttonX() == Button.Pressed) {
+                robot.grabberClutch(GrabberMotorOperation.Open);
+            } else if (control.buttonY() == Button.Pressed) {
+                robot.grabberClutch(GrabberMotorOperation.Close);
+            } else {
+                robot.grabberClutch(GrabberMotorOperation.Off);
             }
-            if (dir.isDown()) {
-                // Pushed down
-                if (robot.isFourBarLowerLimit() == true) {
-                    robot.fourBarMotor(FourBarDirection.Off);
-                } else {
-                    robot.fourBarMotor(FourBarDirection.Down);
+        }
+
+        void linearSlide () {
+            while (true) { // the robot over the baseplate :)
+                Direction dpadDirection = control.dpad();
+                if (dpadDirection.isRight()) {
+                    if (robot.isLinearSlideFullyExtended() == true) {
+                        robot.lslide(LinearSlideOperation.Off);
+                    } else if (robot.isLinearSlideFullyRetracted() == true) {
+                        robot.lslide(LinearSlideOperation.Off);
+                    } else {
+                        robot.lslide(LinearSlideOperation.Extend);
+                    }
+                } else if (dpadDirection.isLeft()) {
+                    if (robot.isLinearSlideFullyExtended() == true) {
+                        robot.lslide(LinearSlideOperation.Off);
+                    } else if (robot.isLinearSlideFullyRetracted() == true) {
+                        robot.lslide(LinearSlideOperation.Off);
+                    } else {
+                        robot.lslide(LinearSlideOperation.Retract);
+                    }
                 }
             }
         }
+
+        void lift () {
+            while (true) {
+                Direction dir = control.lstick();
+                if (dir.isUp()) {
+                    // Pushed up:
+                    if (robot.isFourBarUpperLimit() == true) {
+                        // HIT THEM LIMIT!
+                        robot.fourBarMotor(FourBarDirection.Off);
+                    } else {
+                        robot.fourBarMotor(FourBarDirection.Up);
+                    }
+                }
+                if (dir.isDown()) {
+                    // Pushed down
+                    if (robot.isFourBarLowerLimit() == true) {
+                        robot.fourBarMotor(FourBarDirection.Off);
+                    } else {
+                        robot.fourBarMotor(FourBarDirection.Down);
+                    }
+                }
+            }
+        }
     }
-}
