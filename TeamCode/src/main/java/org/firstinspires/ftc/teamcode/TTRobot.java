96cdcbd4f7dcd361f2cc4da62440749210ac3e2b
s-tiotuicor <s-tiotuicor@bsd405.org>
2019-11-09 11:52:42 -0800

    adding support for snap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TTRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TTRobot.java
@@ -476,8 +476,18 @@ public class TTRobot {
   }
 
   // Turn the robot to a specific angle
-  public void snap(double angle) {
-    //drive(0.0, 0.0, 0.0, angle);
+  public void snap(double targetAngle) {
+    double curAngle = this.gyroHeading();
+    double rotationAngle = curAngle - targetAngle;
+    //Finding fastest way to get to angle
+    rotationAngle = Math.mod(x - 180, 360) - 180
+    double Y = 0;
+    if (rotationAngle < 0) {
+      Y = -1.0
+    } else if (rotationAngle > 0) {
+      Y = 1.0;
+    }
+   joystickDrive(Direction.None, new Direction (0,Y), curAngle);
   }
 
   public void timeDrive(double speed, double time, double angle) {
