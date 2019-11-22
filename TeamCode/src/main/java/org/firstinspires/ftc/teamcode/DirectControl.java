da47b9b19ca59de218d039213e36f43c11954469
rs.funnybone@gmail.com <kit-kat4256>
2019-11-21 16:14:40 -0800

    Auto Grab

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DirectControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DirectControl.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -49,6 +50,19 @@ public class DirectControl extends LinearOpMode {
       } else if (control.rbump() == Button.Pressed) {
         robot.turnn(1);
         telemetry.addLine("Close 0.6");
+      }
+
+      if (control.buttonY() == Button.Pressed) {
+        robot.setLinearSlideDirection(LinearSlideOperation.Extend);
+        robot.claw(TTRobot.CLAWOPENPOSITION);
+        while (robot.)
+        robot.setLift(-1.0);
+        robot.claw(TTRobot.CLAWCLOSEPOSITION);
+        robot.setLift(1.0);
+
+
+
+
       }
 
       // Override the linear slide limit switches
